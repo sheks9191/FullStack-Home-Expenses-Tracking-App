074e95184d0cbe2a773728e2c9cebc7a7fa339c4
Olumide Oluwasegun Bola <sekooniolumide@yahoo.com>
2025-05-17 09:12:27 +0100

    Update Expenses.jsx

--- frontend/src/pages/Expenses.jsx
+++ frontend/src/pages/Expenses.jsx
@@ -7,25 +7,30 @@ import { customAPI } from "../utils/utils"
 
 const expenseUrl = '/expense'
 
-const expensesQuery =(params,store) => {
-  const {search,page,sort} = params
-  return {
-    queryKey:['expenses',search ?? '',page ?? '',sort ?? ''],
-    queryFn:() => customAPI.get(expenseUrl,{
+// const expensesQuery =(params,store) => {
+//   const {search,page,sort} = params
+//   return {
+//     queryKey:['expenses',search ?? '',page ?? '',sort ?? ''],
+//     queryFn:() => customAPI.get(expenseUrl,{
+//       params,
+//       headers:{
+//       authorization:`Bearer ${store.getState().ui.user.loginUserToken}`
+//     }
+//   })
+//   }
+// }
+
+export const loader =(store) => async ({request}) => {
+  const searchParam = new URL(request.url).searchParams.entries();
+  
+  const params = Object.fromEntries([...searchParam]);
+
+    const response = await customAPI.get(expenseUrl,{
       params,
       headers:{
       authorization:`Bearer ${store.getState().ui.user.loginUserToken}`
     }
   })
-  }
-}
-
-export const loader =(queryClient,store) => async ({request}) => {
-  const searchParam = new URL(request.url).searchParams.entries();
-  
-  const params = Object.fromEntries([...searchParam]);
-
-    const response = await queryClient.ensureQueryData(expensesQuery(params,store))
     
     return response.data
 }
