e5712eac617065beda10bd8fa942c9967d01ff1e
Olumide Oluwasegun Bola <sekooniolumide@yahoo.com>
2025-05-17 08:02:26 +0100

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -39,7 +39,7 @@ const queryClient = new QueryClient({
 
 const router = createBrowserRouter([
   {
-    path:'/',
+    path:'stats/',
     element:<ProtectedRoutes>
         <AppLayout/>
       </ProtectedRoutes>,
@@ -126,7 +126,7 @@ const router = createBrowserRouter([
   },
 
     {
-    path:'/landing',
+    path:'/',
     element:<Landing/>,
     errorElement:<Error/>
   },
