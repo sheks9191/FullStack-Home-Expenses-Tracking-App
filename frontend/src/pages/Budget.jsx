854ad7bd125059b494e4dd5635f2db287bb457a5
Olumide Oluwasegun Bola <sekooniolumide@yahoo.com>
2025-05-17 09:27:21 +0100

    Update Budget.jsx

--- frontend/src/pages/Budget.jsx
+++ frontend/src/pages/Budget.jsx
@@ -2,21 +2,25 @@ import { BudgetExpTrends, MonthlyBudget } from "../components"
 import { BudgetWrapper } from "../styledComponents/BudgetStyle"
 import { customAPI } from "../utils/utils";
 
-const budgetQuery =(store) => {
+// const budgetQuery =(store) => {
   
-  return {
-    queryKey:['budget'],
-    queryFn:() => customAPI.get('/budget',{
+//   return {
+//     queryKey:['budget'],
+//     queryFn:() => customAPI.get('/budget',{
+//     headers:{
+//       authorization:`Bearer ${store.getState().ui.user.loginUserToken}`
+//     }
+//   })
+//   }
+// }
+
+export const loader = (store) => async() => {
+
+  const response = await customAPI.get('/budget',{
     headers:{
       authorization:`Bearer ${store.getState().ui.user.loginUserToken}`
     }
   })
-  }
-}
-
-export const loader = (queryClient,store) => async() => {
-
-  const response = await queryClient.ensureQueryData(budgetQuery(store))
   
   return response.data
 }
